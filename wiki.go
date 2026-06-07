d5122504992ad6a1ab11089eab6ce5fa306e28fd
zxh <zhao_xinhao@foxmail.com>
2026-06-07 19:25:32 +0800

    refactor: 分离模板与数据文件
    - 将模板文件移至 tmpl/ 目录
    - 将页面数据文件移至 data/ 目录
    - 更新相关路径引用

--- wiki.go
+++ wiki.go
@@ -14,12 +14,12 @@ type Page struct {
 }
 
 func (p *Page) save() error {
-	filename := p.Title + ".txt"
+	filename := "data/" + p.Title + ".txt"
 	return os.WriteFile(filename, p.Body, 0600)
 }
 
 func loadPage(title string) (*Page, error) {
-	filename := title + ".txt"
+	filename := "data/" + title + ".txt"
 	body, err := os.ReadFile(filename)
 	if err != nil {
 		return nil, err
@@ -28,7 +28,7 @@ func loadPage(title string) (*Page, error) {
 }
 
 // 全局模板变量，预解析模板文件
-var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+var templates = template.Must(template.ParseFiles("tmpl/edit.html", "tmpl/view.html"))
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 	// 使用预解析的模板变量渲染模板
